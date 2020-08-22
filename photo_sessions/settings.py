a9666fd10a2118c90a82c2f9730b6b1ac30d071b
Maja-Jer <maja.jercic@gmail.com>
2020-08-22 19:12:14 +0000

    added checkout views and templates

--- photo_sessions/settings.py
+++ photo_sessions/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+ # Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'photo_sessions.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,7 +82,11 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
-
+            
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
