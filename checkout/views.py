b8f514c2dd5860696474394ab22562b626f414ad
Maja-Jer <maja.jercic@gmail.com>
2020-08-23 22:09:00 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -45,9 +45,13 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-        order_form = OrderForm(form_data)
+       order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -59,12 +63,11 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for quantity in item_data['items_by_size'].items():
+                        for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
